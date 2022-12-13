0b7531add1bc774c7d90cc64d1999e669145bf57
caoimhejyoti <caoimhejyoti@gmail.com>
2022-12-13 18:23:22 +0800

    adding notes for future reference

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,7 +5,7 @@ console.log(currentHour);
 let calendarArray = JSON.parse(localStorage.getItem("calendarArray")) || [];
 let eventHour= $(this).attr("id");
 
-
+// MASTER FUNCTION: all other functions within this function to allow for the HTTP page to fully load before any js is triggered.
 $(function () {
 
 // COMPELTE! Function to save calendar event to local storage
@@ -27,7 +27,7 @@ $(function () {
 
   });
 
-  // COMPLETE! function for colourizing hours
+// COMPLETE! function for colourizing hours
   $(".time-block").each(function () {
     // create varible for hours within HTML element
     let hourBlock= parseInt( $(this).attr("id").split("hour-")[1]);
@@ -46,65 +46,39 @@ $(function () {
     };
   });
 
-
-// Function for 
+// COMPLETE! Function for displaying information from the local storage. 
   $(".time-block").each(function () {
     // create varible for hours within HTML element
     let timeBlock= $(this).attr("id");
-    // console.log(timeBlock);
-    // console.log(calendarArray);
+    // console.log(timeBlock); // used to test
+    // console.log(calendarArray); // used to test
 
+    // create variable for hours within stored data
     let localTime = JSON.parse(localStorage.getItem("calendarArray"));
-    
-    // console.log(localTime[0].time);
+        // console.log(localTime[0].time);// used to test
 
+    //loop to idenitify if any objects within the array contain event data. 
     for (let i=0; i<calendarArray.length; i++){
+      // if function to be called when event data is present.
       if (timeBlock==(localTime[i].time)) {
-        // console.log(localTime[i].event);
-        var message = localTime[i].event;
-        // console.log(timeBlock.children[1]);
-        ($("#"+timeBlock).children(".description").text(message)); //FIXME: currently not targeting one child, but all children! 
+        // console.log(localTime[i].event); // used to test
 
-        console.log("working"); //this works
+        var message = localTime[i].event;
+        // console.log(timeBlock.children[1]); // used to test
+
+        // targeting specific child to input event message. 
+        ($("#"+timeBlock).children(".description").text(message)); 
+
+        console.log("working"); //used to cpnfirm this function is operational
       }
     };
   });
 
-
-  // FIXME: Change date format to include ordinal.function to display current date in the header of the page.
+// FIXME: Change date format to include ordinal.function to display current date in the header of the page.
   var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
   timeDisplayEl.text(rightNow);
   
 });
-
-
-// Task 1
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-
-
-// Task 2
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
-  
-// Task 3
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-
-
-// Task 4
-  //  Add code to display the current date in the header of the page.
 
 // possible development
   // clearBtn.addEventListener('click', function(e){
